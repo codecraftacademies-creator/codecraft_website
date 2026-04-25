433c7ebb62893b45ed3e8f08b908c6ded7164b76
codecraftacademies-creator <codecraft.academies@gmail.com>
2026-04-26 02:56:04 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  images: {
+    unoptimized: true, // important for GitHub Pages
+  },
+
+  // Only needed if your repo is NOT username.github.io
+  basePath: "/your-repo-name",
+  assetPrefix: "/your-repo-name/",
 };
 
 export default nextConfig;
